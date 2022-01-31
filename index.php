ce122867ffbe4a5b4b7503adfe2929524ec4e32a
Abbas <51082367+haghighisina@users.noreply.github.com>
2022-01-31 17:52:31 +0330

    register and login with error and success messages

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php session_start();
 require_once __DIR__.'/includes.php';
-
 $bootstrap = new Bootstrap($_GET);
 $controller = $bootstrap->createController();
 isset ($controller) ?  $controller->executeAction():'';
